3e24b00a063efff65152f0ef4bb45e1d94d80e4e
Sebastian Stauch <bugsteinpeter@gmail.com>
2020-07-03 08:26:56 +0200

    prepare for autobuild

---
+++ cmd/rexecutor/main.go
@@ -0,0 +1,219 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"time"
+
+	nested "github.com/antonfisher/nested-logrus-formatter"
+	"github.com/dchest/uniuri"
+	"github.com/gin-gonic/gin"
+	"github.com/sebidude/configparser"
+	log "github.com/sirupsen/logrus"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+type Job struct {
+	JobID      string         `json:"jobID" yaml:"jobID"`
+	Endpoint   EndpointConfig `json:"endpoint" yaml:"endpoint"`
+	ExitCode   int            `json:"exitCode" yaml:"exitCode"`
+	Running    bool           `json:"running" yaml:"running"`
+	OutputPipe bytes.Buffer   `json:"-"`
+	Output     string         `json:"output" yaml:"output"`
+	Pid        int            `json:"pid" yaml:"pid"`
+}
+
+type Jobs map[string]*Job
+
+type EndpointConfig struct {
+	Path       string   `json:"path" yaml:"path"`
+	AllowMulti bool     `json:"allowMulti" yaml:"allowMulti"`
+	Command    string   `json:"command" yaml:"command"`
+	Args       []string `json:"args" yaml:"command"`
+}
+
+type Configuration struct {
+	ListenAddress string           `json:"listenAddress" yaml:"listenAddress"`
+	Endpoints     []EndpointConfig `json:"endpoints" yaml:"endpoints"`
+}
+
+type Rexecutor struct {
+	Config   *Configuration
+	Jobs     Jobs
+	JobsDone chan string
+}
+
+var (
+	appconfig  Configuration
+	configfile string
+	appversion string
+	gitcommit  string
+	buildtime  string
+)
+
+func main() {
+
+	log.SetFormatter(&nested.Formatter{
+		HideKeys:        true,
+		FieldsOrder:     []string{"component", "category"},
+		TimestampFormat: time.RFC3339Nano,
+	})
+
+	app := kingpin.New("rexecutor", "Run command on remote")
+	app.Flag("config", "Full path to the configfile.").Short('c').Default("config.yaml").StringVar(&configfile)
+
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+	log.WithField("component", "main").Infof("==== Rexecutor ====")
+	log.WithField("component", "main").Infof("appversion: %s", appversion)
+	log.WithField("component", "main").Infof("gitcommit:  %s", gitcommit)
+	log.WithField("component", "main").Infof("buildtime:  %s", buildtime)
+	log.WithField("component", "main").Infof("Reading configfile: %s", configfile)
+
+	err := configparser.ParseYaml(configfile, &appconfig)
+	if err != nil {
+		log.Println(configfile)
+		log.Fatal(err)
+	}
+	log.WithField("component", "main").Infof("Setting config parameters from env.")
+	configparser.SetValuesFromEnvironment("RCE", &appconfig)
+
+	rce := new(Rexecutor)
+	rce.Config = &appconfig
+	rce.Jobs = make(Jobs)
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.New()
+	router.Use(gin.Recovery())
+	router.Use(GinLogger())
+	log.WithField("component", "router").Infof("setting up endpoints")
+	for _, endpoint := range rce.Config.Endpoints {
+		log.WithField("component", "router").Infof("endpoint: %#v", endpoint)
+		router.GET("/run/"+endpoint.Path, rce.runCommand(endpoint))
+	}
+	router.GET("/output/:jobid", rce.jobOutput)
+	router.GET("/result/:jobid", rce.jobResult)
+	router.GET("/status/:jobid", rce.jobStatus)
+	log.WithField("component", "main").Infof("Start server on %s", rce.Config.ListenAddress)
+
+	err = router.Run(rce.Config.ListenAddress)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	log.Println("Shutdown.")
+	os.Exit(0)
+
+}
+
+func (rce *Rexecutor) runCommand(endpoint EndpointConfig) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		rid := uniuri.NewLen(8)
+		c.Set("id", rid)
+		for _, v := range rce.Jobs {
+			if v.Endpoint.Path == endpoint.Path && !v.Endpoint.AllowMulti {
+				log.WithField("component", "runner").Errorf("%s - A job for %s is already running", rid, endpoint.Path)
+				c.String(409, "A job for this endpoint is already running.")
+				c.Abort()
+				return
+			}
+		}
+
+		job := new(Job)
+		job.Endpoint = endpoint
+		job.JobID = rid
+		rce.Jobs[rid] = job
+
+		log.WithField("component", "runner").Infof("%s - exec: %#v", rid, endpoint)
+		cmd := exec.Command(endpoint.Command, endpoint.Args...)
+		cmdout, _ := cmd.StdoutPipe()
+		jw := bufio.NewWriter(&job.OutputPipe)
+		job.Running = true
+		cmd.Start()
+		io.Copy(jw, cmdout)
+		job.Pid = cmd.Process.Pid
+		if err := cmd.Wait(); err != nil {
+			log.WithField("component", "runner").Errorf("%s - job failed: %s", rid, err.Error())
+			c.String(500, "Job failed: %s", err.Error())
+			c.Abort()
+			return
+		}
+		job.Running = false
+		job.ExitCode = cmd.ProcessState.ExitCode()
+		log.WithField("component", "runner").Infof("%s - exec: %#v", rid, endpoint)
+		c.String(200, "Job %s finished.\n%s", rid, job.OutputPipe.String())
+		delete(rce.Jobs, rid)
+	}
+}
+
+func (rce *Rexecutor) jobResult(c *gin.Context) {
+	jobID := c.Param("jobid")
+	rid := uniuri.NewLen(8)
+	c.Set("id", rid)
+
+	if j, ok := rce.Jobs[jobID]; ok {
+		j.Output = j.OutputPipe.String()
+		c.JSON(200, gin.H{
+			"message":   "Job results",
+			"requestID": rid,
+			"job":       j,
+		})
+	} else {
+		c.JSON(404, gin.H{
+			"message":   "No job found",
+			"requestID": rid,
+			"jobID":     jobID,
+		})
+	}
+}
+
+func (rce *Rexecutor) jobOutput(c *gin.Context) {
+	jobID := c.Param("jobid")
+	rid := uniuri.NewLen(8)
+	c.Set("id", rid)
+	if j, ok := rce.Jobs[jobID]; ok {
+		c.String(200, j.OutputPipe.String())
+	} else {
+		c.String(404, "No job found with id: %s", jobID)
+	}
+}
+
+func (rce *Rexecutor) jobStatus(c *gin.Context) {
+	jobID := c.Param("jobid")
+	rid := uniuri.NewLen(8)
+	c.Set("id", rid)
+	if j, ok := rce.Jobs[jobID]; ok {
+		if j.Running {
+			c.String(200, "Running")
+		} else {
+			c.String(200, "Finished")
+		}
+
+	} else {
+		c.String(404, "No job found with id: %s", jobID)
+	}
+}
+
+func GinLogger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := time.Now()
+		c.Next()
+		rid, _ := c.Get("id")
+		// after request
+		latency := time.Since(t)
+
+		// access the status we are sending
+		status := c.Writer.Status()
+		logstring := fmt.Sprintf("%s - %s - %d - %s (%s)",
+			rid,
+			c.Request.RemoteAddr,
+			status,
+			c.Request.RequestURI,
+			latency)
+
+		log.WithField("component", "router").Infof(logstring)
+
+	}
+}
--- go.mod
+++ go.mod
@@ -1,17 +1,22 @@
 module github.com/sebidude/rexecutor
 
+go 1.14
+
 require (
-	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
-	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
-	github.com/dchest/uniuri v0.0.0-20160212164326-8902c56451e9
-	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3 // indirect
-	github.com/gin-gonic/gin v1.3.0
-	github.com/golang/protobuf v1.3.1 // indirect
-	github.com/mattn/go-isatty v0.0.7 // indirect
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/antonfisher/nested-logrus-formatter v1.1.0
+	github.com/dchest/uniuri v0.0.0-20200228104902-7aecb25e1fe5
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator/v10 v10.3.0 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/sebidude/configparser v1.0.0
-	github.com/ugorji/go/codec v0.0.0-20190320090025-2dc34c0b8780 // indirect
-	golang.org/x/sys v0.0.0-20190322080309-f49334f85ddc // indirect
+	github.com/sirupsen/logrus v1.6.0
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
